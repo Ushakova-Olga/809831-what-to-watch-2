25026492c541c14c23d8b4ceaeb74a63b02f790b
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2019-10-24 17:22:17 +0700

    Добавляет моковые данные для описания фильмов

---
+++ src/mocks/films.js
@@ -0,0 +1,24 @@
+const films = [
+  {
+    name: `Fantastic Beasts: The Crimes of Grindelwald`,
+    img: `fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  },
+  {
+    name: `Fantastic Beasts`,
+    img: `bohemian-rhapsody.jpg`,
+  },
+  {
+    name: `Macbeth`,
+    img: `macbeth.jpg`,
+  },
+  {
+    name: `Aviator`,
+    img: `aviator.jpg`,
+  },
+  {
+    name: `We need to talk about Kevin`,
+    img: `we-need-to-talk-about-kevin.jpg`,
+  },
+];
+
+export default questions;
