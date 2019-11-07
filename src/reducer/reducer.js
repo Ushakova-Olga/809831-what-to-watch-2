6bbb107336796b593929491627eff55625ba110b
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2019-11-07 13:19:42 +0700

    Добавляет функцию-редьюсер

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -20,8 +20,25 @@ const ActionCreator = {
         if (genre === `All genres`) {
           return films;
         }
-        return films.filter.filter((it) => it.genre === genre);
+        return films.filter((it) => it.genre === genre);
       }
     };
   }
 };
+
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case `SET_GENRE`:
+      return Object.assign({}, state, {
+        genre: action.payload
+      });
+    case `FILMS_FILTER`:
+      return Object.assign({}, state, {
+        films: action.payload
+      });
+  }
+  return state;
+};
+
+export {reducer, ActionCreator};
