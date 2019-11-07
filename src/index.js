53708b81b9590f9ad6dafc8a2c376e9c097c8ca6
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2019-11-07 13:24:57 +0700

    Инициализирует новое хранилище в index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./reducer/reducer";
+
 import App from "./components/app/app.jsx";
 import films from "./mocks/films.js";
 
-const init = () => {
 
+const init = (filmsList) => {
+  const store = createStore(reducer);
   ReactDOM.render(
-      <App
-        films={films}
-      />,
+      <Provider store={store}>
+        <App
+          films={filmsList}
+        />
+      </Provider>,
       document.querySelector(`#root`)
   );
 };
 
-init();
+init(films);
