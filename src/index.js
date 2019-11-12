a678bb0deaa930e9129c3c67e0d019880dafc75c
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2019-11-12 14:52:31 +0700

    Обновляет код инициализации хранилища Redux

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import {reducer} from "./reducer/reducer";
 import App from "./components/app/app.jsx";
 
 const init = () => {
-  const store = createStore(reducer);
+  const store = createStore(
+      reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+  );
+
   ReactDOM.render(
       <Provider store={store}>
         <App
