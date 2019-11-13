90aa686eaa861b80f459813ac731fd3366a9b6e9
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2019-11-13 09:23:39 +0700

    Добавляет тест для HOC WithActiveItem

---
+++ src/hoc/with-active-item/with-active-item.e2e.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Enzyme, {mount} from 'enzyme';
+import withActiveItem from './with-active-item.jsx';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
+
+const MockComponent = () => <div />;
+const MockComponentWrapped = withActiveItem(MockComponent);
+const activeItem = mount(<MockComponentWrapped />);
+
+it(`WithActiveItem первоначально возвращает false`, () => {
+  expect(activeItem.state().isActive).toEqual(false);
+});
+
+it(`WithActiveItem при наведении переключает isActive`, () => {
+  activeItem.instance().overMouseHandler();
+  expect(activeItem.state().isActive).toEqual(true);
+});
