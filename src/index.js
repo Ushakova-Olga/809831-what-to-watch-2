a17ca3291e74e6394ef0695703aa96545a9368bc
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2019-11-25 13:13:34 +0700

    Подключает BrowserRouter в index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {reducer, LoadFromServer} from "./reducer/reducer";
+import {BrowserRouter} from 'react-router-dom';
 
 import App from "./components/app/app.jsx";
 
