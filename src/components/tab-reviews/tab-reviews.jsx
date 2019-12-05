add79bbfe0cc8452a79d748d9dd1661c3bc84739
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2019-12-05 12:20:24 +0700

    Делает сортировку по датам в отзывах

--- src/components/tab-reviews/tab-reviews.jsx
+++ src/components/tab-reviews/tab-reviews.jsx
@@ -9,54 +9,56 @@ class TabReviews extends React.PureComponent {
 
   render() {
     const {currentTab, indexTab, comments} = this.props;
-
+    const commentsSorted = comments.sort((a, b) => moment(b.date) - moment(a.date));
+    console.log(commentsSorted);
+    const left = [];
+    const right = [];
+    commentsSorted.forEach((it, i, arr) => {
+      if (i % 2 === 0) {
+        left.push(it);
+      } else {
+        right.push(it);
+      }
+    });
     if (currentTab === indexTab) {
       return <article className="movie-card__reviews movie-card__row">
         <div className="movie-card__reviews-col">
-          {comments.map((it, i, arr) => {
-            if (i < arr.length / 2) {
-              return <div className="review" key={it.id}>
-                <blockquote className="review__quote">
-                  <p className="review__text">
-                    {it.comment}
-                  </p>
+          {left.map((it, i, arr) => {
+            return <div className="review" key={it.id}>
+              <blockquote className="review__quote">
+                <p className="review__text">
+                  {it.comment}
+                </p>
 
-                  <footer className="review__details">
-                    <cite className="review__author">{it.user.name}</cite>
-                    <time className="review__date" dateTime={moment(it.date).format(`YYYY-MM-DD`)}>
-                      {moment(it.date).format(`MMMM DD, YYYY`)}
-                    </time>
-                  </footer>
-                </blockquote>
-                <div className="review__rating">{it.rating}</div>
-              </div>;
-            } else {
-              return ``;
-            }
+                <footer className="review__details">
+                  <cite className="review__author">{it.user.name}</cite>
+                  <time className="review__date" dateTime={moment(it.date).format(`YYYY-MM-DD`)}>
+                    {moment(it.date).format(`MMMM DD, YYYY`)}
+                  </time>
+                </footer>
+              </blockquote>
+              <div className="review__rating">{it.rating}</div>
+            </div>;
           })}
         </div>
         <div className="movie-card__reviews-col">
-          {comments.map((it, i, arr) => {
-            if (i >= arr.length / 2) {
-              return <div className="review" key={it.id}>
-                <blockquote className="review__quote">
-                  <p className="review__text">
-                    {it.comment}
-                  </p>
+          {right.map((it, i, arr) => {
+            return <div className="review" key={it.id}>
+              <blockquote className="review__quote">
+                <p className="review__text">
+                  {it.comment}
+                </p>
 
-                  <footer className="review__details">
-                    <cite className="review__author">{it.user.name}</cite>
-                    <time className="review__date" dateTime={moment(it.date).format(`YYYY-MM-DD`)}>
-                      {moment(it.date).format(`MMMM DD, YYYY`)}
-                    </time>
-                  </footer>
-                </blockquote>
+                <footer className="review__details">
+                  <cite className="review__author">{it.user.name}</cite>
+                  <time className="review__date" dateTime={moment(it.date).format(`YYYY-MM-DD`)}>
+                    {moment(it.date).format(`MMMM DD, YYYY`)}
+                  </time>
+                </footer>
+              </blockquote>
 
-                <div className="review__rating">{it.rating}</div>
-              </div>;
-            } else {
-              return ``;
-            }
+              <div className="review__rating">{it.rating}</div>
+            </div>;
           })}
         </div>
       </article>;
