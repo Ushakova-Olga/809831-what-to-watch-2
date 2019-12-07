599abdcb487a9cfe34715e93e3adba20df59ce39
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2019-12-07 13:49:09 +0700

    Добавляет снепшот-тест для компонента Details

---
+++ src/components/details/details.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Details from "./details";
+import {BrowserRouter} from "react-router-dom";
+import films from "../../mocks/films.js";
+import comments from "../../mocks/comments";
+
+it(`FavoriteList component renders correctly`, () => {
+  const tree = renderer
+    .create(<BrowserRouter>
+      <Details
+        activeFilm={1}
+        films={films}
+        clickHandler={() => {}}
+        isAuthorizationRequired={true}
+        clickFavoriteHandler={() => {}}
+        userData={{id: 1, name: `fake`, email: `test@mail.ru`, avatarUrl: ``}}
+        openCloseFilm={() => {}}
+        comments={comments}
+        history={{push: () => {}}}
+      >
+      </Details>
+    </BrowserRouter>)
+  .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
